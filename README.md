175d0a84f90a7faa341c7448121956417ee27cc3
qyvlik <qyvlik@qq.com>
2023-03-27 10:58:20 +0800

    feat: init project

---
+++ README.md
@@ -0,0 +1,6 @@
+
+
+# ref
+
+1. https://github.com/acheong08/ChatGPT
+2. https://github.com/meng-luo/ChatGPT
---
+++ src/main.py
@@ -0,0 +1,45 @@
+
+from revChatGPT.V1 import Chatbot
+import flask
+from flask import request
+import json
+import os
+
+config={
+    "proxy": os.getenv('HTTP_PROXY') or None,
+    "access_token": os.getenv('CHATGPT_ACCESS_TOKEN') or None
+}
+
+server = flask.Flask(__name__)
+chatbot = Chatbot(config, conversation_id=None)
+
+def chat(msg):
+    prompt = msg
+    response = ""
+    for data in chatbot.ask(
+            prompt
+    ):
+        response = data["message"]
+    print(response)
+    return response
+
+@server.route('/chat', methods=['post'])
+def chatapi():
+    requestJson = request.get_data()
+    if requestJson is None or requestJson == "" or requestJson == {}:
+        resu = {'code': 1, 'msg': '请求内容不能为空'}
+        return json.dumps(resu, ensure_ascii=False)
+    data = json.loads(requestJson)
+    print(data)
+    try:
+        msg = chat(data['msg'])
+    except Exception as error:
+        print("接口报错")
+        resu = {'code': 1, 'msg': '请求异常: ' + str(error)}
+        return json.dumps(resu, ensure_ascii=False)
+    else:
+        resu = {'code': 0, 'data': msg}
+        return json.dumps(resu, ensure_ascii=False)
+
+if __name__ == '__main__':
+    server.run(port=7777, host='0.0.0.0')
